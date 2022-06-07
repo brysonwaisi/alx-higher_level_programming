60af54174b952b66e273062112115ddffc3160ec
brysonwaisi <brysonnyamwange@gmail.com>
2022-06-07 08:16:29 -0700

    Technical Interview Prep

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,43 @@
+#include <stdlib.h>
 #include "lists.h"
 
-listint_t *reverse_listint(listint_t **head);
-int is_palindrome(listint_t **head);
-
 /**
- * reverse_listint - Reverses a singly-linked listint_t list.
- * @head: A pointer to the starting node of the list to reverse.
+ * is_palindrome - a function
+ * @head: the list
  *
- * Return: A pointer to the head of the reversed list.
+ * Return: 0 or 1
  */
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *node = *head, *next, *prev = NULL;
 
-	while (node)
-	{
-		next = node->next;
-		node->next = prev;
-		prev = node;
-		node = next;
-	}
-
-	*head = prev;
-	return (*head);
-}
-
-/**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: A pointer to the head of the linked list.
- *
- * Return: If the linked list is not a palindrome - 0.
- *         If the linked list is a palindrome - 1.
- */
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp, *rev, *mid;
-	size_t size = 0, i;
+	listint_t *ptr1, *ptr2, *list_inver, *new;
 
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
-
-	tmp = *head;
-	while (tmp)
-	{
-		size++;
-		tmp = tmp->next;
-	}
-
-	tmp = *head;
-	for (i = 0; i < (size / 2) - 1; i++)
-		tmp = tmp->next;
-
-	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+	ptr1 = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (0);
+	new->n = ptr1->n;
+	new->next = NULL;
+	list_inver = new;
 
-	tmp = tmp->next->next;
-	rev = reverse_listint(&tmp);
-	mid = rev;
-
-	tmp = *head;
-	while (rev)
+	ptr1 = ptr1->next;
+	while (ptr1 != NULL)
 	{
-		if (tmp->n != rev->n)
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
 			return (0);
-		tmp = tmp->next;
-		rev = rev->next;
+		new->next = list_inver;
+		list_inver = new;
+		ptr1 = ptr1->next;
 	}
-	reverse_listint(&mid);
+
+	ptr1 = *head;
+	ptr2 = list_inver;
+	while (ptr1->n == ptr2->n && ptr1->next != NULL && ptr2->next != NULL)
+	if (ptr1->next != NULL || ptr2->next != NULL)
+		return (0);
+	else if (ptr1->n != ptr2->n)
+		return (0);
 
 	return (1);
 }
