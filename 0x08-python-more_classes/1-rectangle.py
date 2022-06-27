555f2c2d17ec1f0ec2adb25c357f18b47b58c4e9
brysonwaisi <brysonnyamwange@gmail.com>
2022-06-27 09:02:16 -0700

    real definition

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-
 """Defines a Rectangle class."""
 
 
 class Rectangle:
     """Represent a rectangle."""
 
-    def __init__(self, height=0, width=0):
+    def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
         Args:
-            height (int): The height of the new rectangle.
             width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
