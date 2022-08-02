c46db62ac398a9b34fd0e42f510c8cd40951c08a
brysonwaisi <brysonnyamwange@gmail.com>
2022-08-02 00:08:17 -0700

    Fix errors in file

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx >= 0 and idx < len(my_list):
+    """ Retrieve an element from a list."""
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
+    else:
         return my_list[idx]
