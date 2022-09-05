782431f043a17298266db8e47ca905c251ddc9b0
brysonwaisi <brysonnyamwange@gmail.com>
2022-09-05 10:15:05 -0700

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (let line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
 	console.log(line);
 }
